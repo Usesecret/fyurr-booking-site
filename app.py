232809a9371d6a1dc927a900e6305dcf40e1f25d
Saida DiPeri <sdiperi@Saidas-MBP.cable.rcn.com>
2020-05-03 04:01:39 -0400

    added association object and created relationship and migrated database

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -21,23 +22,38 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 # TODO: connect to a local postgresql database
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sdiperi@localhost:5432/fyyur'
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
+
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.Integer()), nullable=False)
+    website_link = db.Column(db.String(120), nullable=False)
+    seeking_talent = db.Column(db.Boolean, nullable = False)
+    seeking_description = db.Column(db.String, nullable=False)
+    past_shows = db.Column(db.ARRAY(db.Integer()), nullable=False)
+    upcoming_shows = db.Column(db.ARRAY(db.Integer()), nullable=False)
+    artists = db.relationship('Artist', secondary = 'Artist')
+
+    
+    
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -45,15 +61,37 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=False)
+    seeking_venues = db.Column(db.String(120), nullable=False)
+    website_link = db.Column(db.String(120), nullable=False)
+    seeking_artists = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.Integer()), nullable=False)
+    seeking_description = db.Column(db.String, nullable=False)
+    past_shows = db.Column(db.ARRAY(db.Integer()), nullable=False)
+    
+    
+    venues = db.relationship('Venue', secondary='Venue')
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
+class Show(db.Model):
+    __tablename__='show'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+    dttm = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    venue = db.relationship('Venue', back_populates="artists")
+    product = db.relationship('Artist', back_populates="venues")
+
+# db.create_all()
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
